78ae3aa848878489814e3bfbbc4fe6b7f930cbba
lucas-callado <Lucas.Callado@dynatrace.com>
2016-10-09 01:17:07 -0400

    commit2

---
+++ Assets/Scripts/CubeScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void updateCubePosition(float x, float y){
+		//float x1 = (float)x;
+		//float y1 = (float)y;
+
+		transform.position = new Vector3(x, y);
+	}
+}
